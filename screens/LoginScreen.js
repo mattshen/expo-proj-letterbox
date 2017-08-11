ff041f5f02371af573d1581ad0157ff0fd17532c
Matt Shen <lovelywib@gmail.com>
2017-08-12 00:43:09 +1000

    added login screen

---
+++ screens/LoginScreen.js
@@ -0,0 +1,70 @@
+import React, { Component } from 'react';
+import { View, StyleSheet, Button, Alert } from 'react-native';
+import { Constants, Google } from 'expo';
+
+export default class LoginScreen extends Component {
+
+  _navToMain = () => {
+    this.props.navigation.navigate('Main');
+  }
+
+  _handleGoogleLogin = async () => {
+    try {
+      const { type, user } = await Google.logInAsync({
+        androidStandaloneAppClientId: '<ANDROID_CLIENT_ID>',
+        iosStandaloneAppClientId: '<IOS_CLIENT_ID>',
+        androidClientId: '603386649315-9rbv8vmv2vvftetfbvlrbufcps1fajqf.apps.googleusercontent.com',
+        iosClientId: '603386649315-vp4revvrcgrcjme51ebuhbkbspl048l9.apps.googleusercontent.com',
+        scopes: ['profile', 'email']
+      });
+
+      switch (type) {
+        case 'success': {
+          Alert.alert(
+            'Logged in!',
+            `Hi ${user.name}!`,
+          );
+          this._navToMain();
+          break;
+        }
+        case 'cancel': {
+          Alert.alert(
+            'Cancelled!',
+            'Login was cancelled!',
+          );
+          break;
+        }
+        default: {
+          Alert.alert(
+            'Oops!',
+            'Login failed!',
+          );
+        }
+      }
+    } catch (e) {
+      Alert.alert(
+        'Oops!',
+        'Login failed!',
+      );
+    }
+  };
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Button title="Login with Google" onPress={this._handleGoogleLogin} />
+        <Button title="Goto Main" onPress={this._navToMain} />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingTop: Constants.statusBarHeight,
+    backgroundColor: '#ecf0f1',
+  },
+});
