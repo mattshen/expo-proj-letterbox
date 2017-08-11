eec40104e09f19814083070abad480629108d99f
Matt Shen <lovelywib@gmail.com>
2017-08-12 00:14:39 +1000

    apply project template

--- App.js
+++ App.js
@@ -1,77 +1,69 @@
-// Exported from snack.expo.io
-import React, { Component } from 'react';
-import { View, Text, StyleSheet, Button, Alert } from 'react-native';
-import { Constants, Google } from 'expo';
+import React from 'react';
+import { Platform, StatusBar, StyleSheet, View } from 'react-native';
+import { AppLoading, Asset, Font } from 'expo';
+import { Ionicons } from '@expo/vector-icons';
+import RootNavigation from './navigation/RootNavigation';
 
-export default class App extends Component {
-  _handleButtonPress = () => {
-    Alert.alert(
-      'Button pressed!',
-      'You did it!',
-    );
+export default class App extends React.Component {
+  state = {
+    assetsAreLoaded: false,
   };
 
-  _handleGoogleLogin = async () => {
-    try {
-      const { type, user } = await Google.logInAsync({
-        androidStandaloneAppClientId: '<ANDROID_CLIENT_ID>',
-        iosStandaloneAppClientId: '<IOS_CLIENT_ID>',
-        androidClientId: '603386649315-9rbv8vmv2vvftetfbvlrbufcps1fajqf.apps.googleusercontent.com',
-        iosClientId: '603386649315-vp4revvrcgrcjme51ebuhbkbspl048l9.apps.googleusercontent.com',
-        scopes: ['profile', 'email']
-      });
-
-      switch (type) {
-        case 'success': {
-          Alert.alert(
-            'Logged in!',
-            `Hi ${user.name}!`,
-          );
-          break;
-        }
-        case 'cancel': {
-          Alert.alert(
-            'Cancelled!',
-            'Login was cancelled!',
-          );
-          break;
-        }
-        default: {
-          Alert.alert(
-            'Oops!',
-            'Login failed!',
-          );
-        }
-      }
-    } catch (e) {
-      Alert.alert(
-        'Oops!',
-        'Login failed!',
-      );
-    }
-  };
+  componentWillMount() {
+    this._loadAssetsAsync();
+  }
 
   render() {
-    return (
-      <View style={styles.container}>
-        <Text>Hello World</Text>
-        <Button
-          title="Login with Google"
-          onPress={this._handleGoogleLogin}
-        />
-      
-      </View>
-    );
+    if (!this.state.assetsAreLoaded && !this.props.skipLoadingScreen) {
+      return <AppLoading />;
+    } else {
+      return (
+        <View style={styles.container}>
+          {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
+          {Platform.OS === 'android' &&
+            <View style={styles.statusBarUnderlay} />}
+          <RootNavigation />
+        </View>
+      );
+    }
+  }
+
+  async _loadAssetsAsync() {
+    try {
+      await Promise.all([
+        Asset.loadAsync([
+          require('./assets/images/robot-dev.png'),
+          require('./assets/images/robot-prod.png'),
+        ]),
+        Font.loadAsync([
+          // This is the font that we are using for our tab bar
+          Ionicons.font,
+          // We include SpaceMono because we use it in HomeScreen.js. Feel free
+          // to remove this if you are not using it in your app
+          { 'space-mono': require('./assets/fonts/SpaceMono-Regular.ttf') },
+        ]),
+      ]);
+    } catch (e) {
+      // In this case, you might want to report the error to your error
+      // reporting service, for example Sentry
+      console.warn(
+        'There was an error caching assets (see: App.js), perhaps due to a ' +
+          'network timeout, so we skipped caching. Reload the app to try again.'
+      );
+      console.log(e);
+    } finally {
+      this.setState({ assetsAreLoaded: true });
+    }
   }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    paddingTop: Constants.statusBarHeight,
-    backgroundColor: '#ecf0f1',
+    backgroundColor: '#fff',
+  },
+  statusBarUnderlay: {
+    height: 24,
+    backgroundColor: 'rgba(0,0,0,0.2)',
   },
 });
-
