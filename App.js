de879208d3344a4b168d25dfb9ccf80c7e2c1c29
Matt Shen <lovelywib@gmail.com>
2017-08-11 23:52:39 +1000

    start project

---
+++ App.js
@@ -0,0 +1,77 @@
+// Exported from snack.expo.io
+import React, { Component } from 'react';
+import { View, Text, StyleSheet, Button, Alert } from 'react-native';
+import { Constants, Google } from 'expo';
+
+export default class App extends Component {
+  _handleButtonPress = () => {
+    Alert.alert(
+      'Button pressed!',
+      'You did it!',
+    );
+  };
+
+  _handleGoogleLogin = async () => {
+    try {
+      const { type, user } = await Google.logInAsync({
+        androidStandaloneAppClientId: '<ANDROID_CLIENT_ID>',
+        iosStandaloneAppClientId: '<IOS_CLIENT_ID>',
+        androidClientId: '603386649315-9rbv8vmv2vvftetfbvlrbufcps1fajqf.apps.googleusercontent.com',
+        iosClientId: '603386649315-vp4revvrcgrcjme51ebuhbkbspl048l9.apps.googleusercontent.com',
+        scopes: ['profile', 'email']
+      });
+
+      switch (type) {
+        case 'success': {
+          Alert.alert(
+            'Logged in!',
+            `Hi ${user.name}!`,
+          );
+          break;
+        }
+        case 'cancel': {
+          Alert.alert(
+            'Cancelled!',
+            'Login was cancelled!',
+          );
+          break;
+        }
+        default: {
+          Alert.alert(
+            'Oops!',
+            'Login failed!',
+          );
+        }
+      }
+    } catch (e) {
+      Alert.alert(
+        'Oops!',
+        'Login failed!',
+      );
+    }
+  };
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Hello World</Text>
+        <Button
+          title="Login with Google"
+          onPress={this._handleGoogleLogin}
+        />
+      
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingTop: Constants.statusBarHeight,
+    backgroundColor: '#ecf0f1',
+  },
+});
+
